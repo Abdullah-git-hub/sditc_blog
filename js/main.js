f94a54403d57a8f88f51b616f57ab5b5c08766a3
Abdullah-git-hub <74533464+Abdullah-git-hub@users.noreply.github.com>
2023-01-22 01:41:58 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ const search_inp = document.querySelector("#blog_search"),
 search_btn = document.querySelector(".fa-magnifying-glass"),
 search_close = document.querySelector(".fa-xmark"),
 search_box = document.querySelector(".search_box"),
-mode_switch = document.querySelector(".modes i");
+mode_switch = document.querySelector(".modes i"),
+side_bar_open = document.querySelector(".logo .fa-bars"),
+side_bar_close = document.querySelector(".side_bar_close"),
+side_bar = document.querySelector(".side_bar");
 
 search_close.style.display = "none"
 
@@ -21,3 +24,34 @@ mode_switch.addEventListener("click", _=> {
     mode_switch.classList.toggle("fa-moon");
     document.body.classList.toggle("light_mode");
 })
+
+// side_bar_open
+side_bar_open.addEventListener("click", _=> {
+    side_bar.classList.add("side_bar_open");
+    document.body.style.overflow = "hidden";
+})
+
+document.querySelector(".side_bar_back").addEventListener("click", _=>{
+    side_bar.classList.remove("side_bar_open");
+    document.body.style.overflow = "auto";
+})
+
+side_bar_close.addEventListener("click", _=>{
+    side_bar.classList.remove("side_bar_open");
+    document.body.style.overflow = "auto";
+})
+
+
+
+// working with side bar
+
+$(".side_sub_menu_ul").slideUp();
+$(".side_sub_sub_ul").slideUp();
+
+$(".side_sub_menu").click( function() { 
+    $(this).next("ul").slideToggle(0);
+});
+
+$(".side_sub_sub").click( function() { 
+    $(this).next("ul").slideToggle(0);
+});
